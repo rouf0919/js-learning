3fa7d87661adc8aff1858208ce18f8c39448b9b5
Abdul Rauf <112372898+rouf0919@users.noreply.github.com>
2024-05-14 11:01:20 +0000

    data types

---
+++ js-basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+// alert(3 + 3) // we are using nodesjs, not browser
+console.log(3 
+    + 3)  // code readability should be high
+console.log("Rauf")
+
+let age = 35 //number => 2 to power 53
+// bigint
+let name = "rouf"  //String  => ""
+let isLoggedIn = false //Boolean => true/false
+// null => standalone value
+// undefined  => not value assigned for now
+// symbol => unique
+
+// object
+
+console.log(typeof "rouf")
+console.log(typeof undefined)
+console.log(typeof null)
