385fa310b8dc298bdb0d38aacdf1bf18e090d7b8
Abdul Rauf <112372898+rouf0919@users.noreply.github.com>
2024-05-14 11:39:05 +0000

    operations

---
+++ js-basics/operation.js
@@ -0,0 +1,32 @@
+let value = 3
+let negativeValue = -value
+console.log(negativeValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "Abdul"
+let str2 = "Rauf"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+// let num1= num2 = num3
+// num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
